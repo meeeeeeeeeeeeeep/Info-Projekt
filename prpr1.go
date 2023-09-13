d16e52e5291ba0a41733af5dd7c7c8c32ce94a2c
Katja <katja.kiseleva@AD.B-SZ-WSGY.LOGODIDACT.NET>
2023-09-13 09:40:04 +0200

    13.9 cases

--- prpr1.go
+++ prpr1.go
@@ -20,19 +20,69 @@ func New(user, name string) *data {
 	return t
 }
 
-func (t *data) String () string {
+func (t *data) String (casenum int) string {
 	var erg string
 	erg = erg + fmt.Sprintln(t.name)
 	erg = erg + fmt.Sprintln(t.user)
-	for i:=0;i<len(t.titel);i++{
-		erg = erg + "("
-		if t.done[i] {
-			erg = erg + "x) "
-		} else {
-			erg = erg + " ) "
+	//Ansicht:alles iwie ungeordnet
+	if casenum <=2{			//Zahl noch bearbeiten
+		switch casenum{
+			case 0://alles eif iwie
+				for i:=0;i<len(t.titel);i++{
+					erg = erg + "("
+					if t.done[i] {
+						erg = erg + "x) "
+					} else {
+						erg = erg + " ) "
+					}
+					erg = erg + t.titel[i] + " "
+					erg = erg + fmt.Sprintln(t.deadline[i])
+				}
+			case 1: //alle false(unerledigt)
+				for i:=0;i<len(t.titel);i++{
+					if t.done[i]=false {
+						erg = erg + "( ) " + t.titel[i] + " "
+						erg = erg + fmt.Sprintln(t.deadline[i])
+					}
+				}
+			case 2: //alle erledigten true
+				for i:=0;i<len(t.titel);i++{
+					if t.done[i] {
+						erg = erg + "(x) " + t.titel[i]
+						erg = erg + fmt.Sprintln(t.deadline[i])
+					}
+				}
+			case 3: //alle geordnet nach done
+				var unerledigtdone []bool
+				var erledigtdone []bool
+				var unerledigttitel []string
+				var erledigttitel []string
+				var unerledigtdeadline []string
+				var erledigtdeadline []string
+				for i:=0;i<len(t.titel);i++{
+					if t.done[i] {
+						erledigtdone = append(erledigtdone, t.done[i])
+						erledigttitel = append(erledigttitel, t.titel[i])
+						erledigtdeadline = append(erledigtdeadline, t.deadline[i])
+					} else{
+						unerledigtdone = append(unerledigtdone, t.done[i])
+						unerledigttitel = append(unerledigttitel, t.titel[i])
+						unerledigtdeadline = append(unerledigtdeadline, t.deadline[i])
+					}
+				t.done = append(t.done, unerledigtdone[], erledigtdone[]...)
+				t.titel = append(t.titel, unerledigttitel[], erledigttitel[]...)
+				t.deadline = append(t.deadline, unerledigtdeadline[], erledigtdeadline[]...)
+				for i:=0;i<len(t.titel);i++{
+					erg = erg + "("
+					if t.done[i] {
+						erg = erg + "x) "
+					} else {
+						erg = erg + " ) "
+					}
+					erg = erg + t.titel[i] + " "
+					erg = erg + fmt.Sprintln(t.deadline[i])
+				}		
 		}
-		erg = erg + t.titel[i] + " "
-		erg = erg + fmt.Sprintln(t.deadline[i])
 	}
 	return erg
 }
@@ -90,7 +140,8 @@ func (t*data) UmschreibenDeadline (i int, newdeadline string){
 	}
 }
 
-//test
+
+
 
 
 
